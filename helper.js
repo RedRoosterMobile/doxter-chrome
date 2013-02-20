dfd3d97cf261effce7ec57b343b017ecbfb5e8e1
Finn Heemeyer <finn.heemeyer@ymail.com>
2013-02-20 16:10:44 +0100

    Added .css file for option page, some styling and api console

--- helper.js
+++ helper.js
@@ -1,10 +1,31 @@
-function connect(args) {
-
+// Function to connect to the API
+function doxConnect(args) {
   $.ajax({
     url: args.baseUrl+"/"+args.path,
     headers: {"Authorization": "Basic "+btoa(args.username+":"+args.password) }, 
     success: function(data) { args.success(data) },
     error: function(data) { args.error(data) } 
   });
+}
 
+
+function showStatus(message, success) {
+  $("#status-inner").html(message)
+  if(success) {
+    $("#status").css("background", "rgb(160, 250, 160)");
+  }
+  else {
+    $("#status").css("background", "rgb(250, 160, 160)");
+  }
+  $("#status").fadeIn("slow");
+  setTimeout(function () {
+    $("#status").fadeOut("slow");
+  }, 1000);
+}
+
+
+function getCredentials() {
+    window.api_base_url = localStorage.getItem("doxter-api-base-url") ? localStorage.getItem("doxter-api-base-url") : "";
+    window.api_username = localStorage.getItem("doxter-api-username") ? localStorage.getItem("doxter-api-username") : "";
+    window.api_password = localStorage.getItem("doxter-api-password") ? localStorage.getItem("doxter-api-password") : "";
 }
--- options.js
+++ options.js
@@ -9,35 +9,51 @@ $(function() {
   });
 
   $("#console-execute").click(function() {
+    consoleExecute();
   });
 
-  var api_base_url = localStorage.getItem("doxter-api-base-url") ? localStorage.getItem("doxter-api-base-url") : "";
-  var username = localStorage.getItem("doxter-api-username") ? localStorage.getItem("doxter-api-username") : "";
-  var password = localStorage.getItem("doxter-api-password") ? localStorage.getItem("doxter-api-password") : "";
+  getCredentials();
   
-  $("#api-base-url").attr("value", api_base_url);
-  $("#username").attr("value", username);
-  $("#password").attr("value", password);
+  $("#api-base-url").attr("value", window.api_base_url);
+  $("#username").attr("value", window.api_username);
+  $("#password").attr("value", window.api_password);
 
   function saveOptions() {
     localStorage.setItem("doxter-api-base-url", $("#api-base-url").val());
     localStorage.setItem("doxter-api-username", $("#username").val());
     localStorage.setItem("doxter-api-password", $("#password").val());
-
-    $("#status").html("Saved :)");
-    $("#saved").fadeIn("slow");
-    $("#saved").fadeOut("slow");
+    
+    showStatus("Saved", true);
+    getCredentials();
   }
 
   function connectionTest() {
-  
-    connect({
-      baseUrl: api_base_url,
+    doxConnect({
+      baseUrl: window.api_base_url,
       path: "calendars",
-      username: username,
-      password: password,
-      success: function(data) { console.log(data); },
-      error: function(data) { console.log(data); }
+      username: window.api_username,
+      password: window.api_password,
+      success: function(data) {
+        showStatus("Success", true); 
+      },
+      error: function(data) { showStatus("Error", false); }
+    });
+  }
+
+  function consoleExecute() {
+    $("span.msg-to-hide").hide();
+    $("img.spinner").fadeIn();
+    
+    doxConnect({
+      baseUrl: window.api_base_url,
+      path: $("#console-input").val(),
+      username: window.api_username,
+      password: window.api_password,
+      success: function(data) {
+        showStatus("Success", true);
+        $("#console-output").html("");
+        $("#console-output").jsonEditor(data);
+      } 
     });
   }
 });
