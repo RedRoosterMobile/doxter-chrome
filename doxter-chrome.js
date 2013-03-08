b645271af40d9d891094a0931667373b12461d85
Finn-Lennart Heemeyer <finn@doxter-F55A.(none)>
2013-03-08 18:48:48 +0100

    Version 1.1.0 - All bugs fixed

--- doxter-chrome.js
+++ doxter-chrome.js
@@ -3,34 +3,13 @@ getSettings();
 
 start();
 
-// Make sure access-token is valid every 20 minutes
-window.setInterval(getAccessToken, 20 * 60 * 1000);
-
 // Needed for browseraction popup display
 window.unconfirmed_bookings = new Array();
 
-// Starts the whole syncing process
+// Start syncing process, check everything
 function start() {
-
   if(window.api_base_url && window.api_username && window.api_password && window.api_doxcal_id) {
-
-    // Get Access-Token from local storage or google 
-    getAccessToken();
-    window.interval = window.api_sync_every ? window.api_sync_every : 60;
-    window.api_gcal_id = window.api_gcal_id ? window.api_gcal_id : 'primary';
-
-    window.setInterval(function() {
-      syncGoogleToDoxter();
-    }, window.interval * 1000);
-
-    // Always sync Google->Doxter first, wait 30 sec
-    window.setTimeout(function() {
-      window.setInterval(function() {
-        syncDoxterToGoogle();
-      }, window.interval * 1000);
-    }, 30 * 1000);
-
-    window.started_syncing = true;
+    getAccessToken(start_);
   }
   else {
     notifyUser("Bitte geben sie auf der Optionsseite ihre Daten ein!", "info48.png");
@@ -39,157 +18,189 @@ function start() {
 }
 
 
-// Sync events from Google to Doxter
-function syncGoogleToDoxter() {
+// Actually starts the process, internal function
+function start_() {
+  window.setInterval(function() {
+    getAccessToken(function() {
+      sync();
+    });
+  }, window.api_sync_every * 1000);
 
-  var url = "https://www.googleapis.com/calendar/v3/calendars/" + window.api_gcal_id + "/events";
-  var params = {
-    "updatedMin": (new Date(parseInt(window.api_last_synced_gtod))).toISOString(),
-  }
-
-
-  // Get events
-  $.ajax({
-    url: url + "?" + stringify(params),
-    headers: {
-      "Authorization": "OAuth " + window.access_token
-    },
-    success: function(data) {
-      if(!data.items) {
-        return;
-      }
-      for(i = 0; i < data.items.length; i++) {
-        console.log("Saving event from Google to Doxter:");
-        console.log(data.items[i]);
-
-        var message = "Created blocking:";
-        var params = {
-          "starts" : (new Date(data.items[i].start.dateTime)).toISOString(),
-          "ends" : (new Date(data.items[i].end.dateTime)).toISOString()
-        }
-        // If id is found in description, reschedule
-        if(data.items[i].description) {
-          match = data.items[i].description.match(/DXID:(.*)$/);
-          if(match) {
-            params.id = match[1];
-            message = "Rescheduled Doxter event:";
-          }
-        }
-
-        var blocking_id = undefined;
-
-        // Insert blocking
-        doxConnect({
-          baseUrl: window.api_base_url,
-          method: "post",
-          async: false,
-          path: "calendars/"+window.api_doxcal_id + "/events",
-          data: params,
-          username: window.api_username,
-          password: window.api_password,
-          success: function(data) {
-            console.log(message);
-            console.log(data);
-            if(blocking_id != data.id) {
-              console.log("Something went wrong");
-            }
-          },
-          error: function(data) {
-            console.log(data);
-          }
-        });
-
-        // Only save blocking ID, if event wasn't already reschelduled
-        if(!params.id) {
-          addBlockingIdToEvent(blocking_id, data.items[i]);
-        }
-
-      }
-    },
-    error: function(data) {
-      console.log(data);
-    }
-  });
-
-  // Set last synced to now
-  window.api_last_synced_gtod = (new Date()).getTime();
-  localStorage.setItem("doxter-api-last-synced-gtod", window.api_last_synced_gtod);
+  window.started_syncing = true;
 }
 
+// Well, what will this function do?
+function sync() {
+  var last_synced = Date.now();
+  var doxter_data;
+  var google_data;
 
+  getDataFromDoxter(function(data) {
+    doxter_data = data;
+  });
+  getDataFromGoogle(function(data) {
+    google_data = data;
+  });
+  
+  sendDataToDoxter(google_data);
+  sendDataToGoogle(doxter_data);
 
-// Sync bookings from Doxter to Google
-function syncDoxterToGoogle() {
+  window.api_last_synced = last_synced;
+}
 
+// Receive bookings from Doxter
+function getDataFromDoxter(callback) {
   var params = {
-    "updated": new Date(parseInt(window.api_last_synced_dtog)).toISOString()
+    "updated": new Date(parseInt(window.api_last_synced)).toISOString()
   }
 
 
   // Get bookings
   doxConnect({
+    async: false,
     baseUrl: window.api_base_url,
     path: "calendars/"+window.api_doxcal_id + "/events?" + stringify(params),
     username: window.api_username,
     password: window.api_password,
     success: function(data) {
-      var bookings = data;
-      var new_unconfirmed_bookings = new Array();
-
-      for(i = 0; i < bookings.length; i++) {
-        console.log("Saving bookings from Doxter to Google:");
-        console.log(bookings[i]);
-
-        if(bookings[i].confirmation_link) {
-          new_unconfirmed_bookings.push(bookings[i]);
-        }
-        var url = "https://www.googleapis.com/calendar/v3/calendars/" + window.api_gcal_id + "/events";
-        var params = {
-          "start": {
-            "dateTime": (new Date(bookings[i].starts)).toISOString()
-          },
-          "end": {
-            "dateTime": (new Date(bookings[i].ends)).toISOString()
-          },
-          "summary": bookings[i].title,
-          "description": bookings[i].reason + "\n\nDXID:"+bookings[i].id
-        }
-
-        // Insert event
-        $.ajax({
-          url: url,
-          headers: {
-            "Authorization": "OAuth " + window.access_token,
-            "Content-Type": "application/json"
-          },
-          type: "post",
-          dataType: "json",
-          data: JSON.stringify(params),
-          success: function(data) {
-            console.log("Created event:");
-            console.log(data);
-          },
-          error: function(data) {
-            console.log(data);
-          }
-        });
-      }
-
-      if(new_unconfirmed_bookings.length) { 
-        notifyUser(bookings.length+" neue Buchungen auf ihrem Doxter Account!", "info48.png");
-        window.unconfirmed_bookings = window.unconfirmed_bookings.concat(new_unconfirmed_bookings);
-      }
+      callback(data);
     },
     error: function(data) {
       notifyUser("Es konnte keine Verbindung zur doxter API aufgebaut werden!", "error48.png");
     }
   });
-
-  // Set last synced to now
-  window.api_last_synced_dtog = (new Date()).getTime();
-  localStorage.setItem("doxter-api-last-synced-dtog", window.api_last_synced_dtog);
 }
 
+// Receive events from Google
+function getDataFromGoogle(callback) {
+  
+  var url = "https://www.googleapis.com/calendar/v3/calendars/" + window.api_gcal_id + "/events";
+  var params = {
+    "updatedMin": (new Date(parseInt(window.api_last_synced))).toISOString(),
+  }
+
+
+  // Get events
+  $.ajax({
+    async: false,
+    url: url + "?" + stringify(params),
+    headers: {
+      "Authorization": "OAuth " + window.access_token
+    },
+    success: function(data) {
+      callback(data);
+    },
+    error: function(data) {
+      console.log(data);
+    }
+  });
+}
+  
+// Send Google-Data to Doxter
+function sendDataToDoxter(data) {
+  if(!data.items) {
+    return;
+  }
+  for(i = 0; i < data.items.length; i++) {
+    console.log("Saving event from Google to Doxter:");
+    console.log(data.items[i]);
+
+    var message = "Created blocking:";
+    var params = {
+      "starts" : (new Date(data.items[i].start.dateTime)).toISOString(),
+      "ends" : (new Date(data.items[i].end.dateTime)).toISOString()
+    }
+    // If id is found in description, reschedule
+    if(data.items[i].description) {
+      match = data.items[i].description.match(/DXID:(.*)$/);
+      if(match) {
+        params.id = match[1];
+        message = "Rescheduled Doxter event with ID: " + params.id;
+      }
+    }
+    
+    var blocking_id = undefined;
+
+    // Insert blocking
+    doxConnect({
+      baseUrl: window.api_base_url,
+      method: "post",
+      async: false,
+      path: "calendars/"+window.api_doxcal_id + "/events",
+      data: params,
+      username: window.api_username,
+      password: window.api_password,
+      success: function(data) {
+        console.log(message);
+        if(!params.id) {
+          console.log(data);
+        }
+        blocking_id = data.id;
+      },
+      error: function(data) {
+        console.log(data);
+      }
+    });
+
+    // Only save blocking ID, if event wasn't already reschelduled
+    if(!params.id) {
+      addBlockingIdToEvent(blocking_id, data.items[i]);
+    }
+  }
+}
+
+// Send Doxter-Data to Google
+function sendDataToGoogle(data) {
+  var bookings = data;
+
+  for(i = 0; i < bookings.length; i++) {
+    // If there is no confirmation_token, the booking was rescheduled
+    var confirmation_link = bookings[i].confirmation_link;
+    if(!confirmation_link.match(/confirmation_token/)) {
+      continue;
+    }
+    
+    console.log("Saving booking from Doxter to Google:");
+    console.log(bookings[i]);
+
+    notifyUser(
+      bookings.length+' neue Buchungen auf ihrem Doxter Account!<br /><a href="' + confirmation_link + '">Buchung bestätigen</a>',
+      "info48.png"
+    );
+
+    var url = "https://www.googleapis.com/calendar/v3/calendars/" + window.api_gcal_id + "/events";
+    var params = {
+      "start": {
+        "dateTime": (new Date(bookings[i].starts)).toISOString()
+      },
+      "end": {
+        "dateTime": (new Date(bookings[i].ends)).toISOString()
+      },
+      "summary": bookings[i].title,
+      "description": bookings[i].reason + "\n\nDXID:"+bookings[i].id
+    }
+
+    // Insert event
+    $.ajax({
+      async: false,
+      url: url,
+      headers: {
+        "Authorization": "OAuth " + window.access_token,
+        "Content-Type": "application/json"
+      },
+      type: "post",
+      dataType: "json",
+      data: JSON.stringify(params),
+      success: function(data) {
+        console.log("Created event:");
+        console.log(data);
+      },
+      error: function(data) {
+        console.log(data);
+      }
+    });
+  }
+}
 
 // Update Google Calendar entry with Doxter-ID
 function addBlockingIdToEvent(blocking_id, event_) {
